5264d68955b43ba51f2cd804971cca084ad9d679
Manglejuice <julestourrel07@gmail.com>
2026-04-19 18:52:07 -0400

    Add files via upload

---
+++ QueueClassTP.py
@@ -0,0 +1,17 @@
+class QueueClass:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def get_all(self):
+        return self.items
